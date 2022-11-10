af72ccff6ce1250f53095080b7d611a51046b239
Nurbek <nurbekkolbaev81@gmail.com>
2022-11-10 18:24:44 +0600

    base

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,5 +1,5 @@
 ﻿int namberA = new Random().Next(1,10);//123...9
 Console.WriteLine(namberA);
 int namberB = new Random().Next (1,10); 
-Console.WriteLine(namberB);
+Console.Write*Line(namberB);
 Console.WriteLine(namberA+namberB);
