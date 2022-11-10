e8acc7579e1b8752d90e57d6039b0ba7885f3984
Nurbek <nurbekkolbaev81@gmail.com>
2022-11-10 17:35:12 +0600

    nujno ispravit

---
+++ Example005_UserName/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine ();
+if(username.ToLower() ==("Маша");
+{
+Сonsole.WriteLine("Ура, это же МАША");
+}
+else
+Сonsole.Write("Привет, ");
+Сonsole.WriteLine(username);
+}
